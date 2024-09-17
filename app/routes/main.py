6c88d4c1711faa48ca5703952a132c5b0b181670
Vinícius Borges <dev.vinicius.borges@outlook.com>
2024-09-17 20:44:16 -0300

    Atualizar o main.py

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,8 +1,13 @@
 from flask import Blueprint, render_template, request, redirect, session
 from app.models import Veiculo
-from app.middlewares import getAuthResponse
+from app.middleware import AuthMiddleware
 
 main_bp = Blueprint('main', __name__, static_folder='static')
+
+@main_bp.before_request
+def loginAuth():
+    if not AuthMiddlware.is_logged():
+        return redirect(url_for('user'))
 
 @main_bp.route('/')
 @getAuthResponse
